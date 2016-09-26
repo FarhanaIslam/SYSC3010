dbe6441f5848eb44d8b12502bf4ff83a554470a8
admin <admin@gm-PC>
2016-09-26 12:13:01 -0400

    I made a change in branch1

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -32,6 +32,7 @@ public class AddressBook {
 		ab.addBuddy(bi);
 		ab.removeBuddy(0);
 		System.out.println("Editing file on GitHub repository");
+		System.out.println("Editing in branch1");
 	}
 
 }
